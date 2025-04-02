d1d857e045462f355fc250fa23d6d0839377a8b1
CasperDkk <daniel.kamau8@gmail.com>
2025-04-02 16:04:34 +0300

    Check

--- submission/06.sh
+++ submission/06.sh
@@ -1,2 +1,8 @@
 # What is the hash of this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+transaction="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
+
+decoded=$(bitcoin-cli decoderawtransaction $transaction)
+txid=$(echo $decoded | jq -r '.txid')
+echo "Transaction Hash: $txid"
