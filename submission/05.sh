6dbb757644ec36a08cfba8d7e16ddde117c48d6a
CasperDkk <daniel.kamau8@gmail.com>
2025-04-02 16:20:22 +0300

    05 and 06.sh updated

--- submission/05.sh
+++ submission/05.sh
@@ -5,23 +5,23 @@
 # transaction="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
 
 
-recipient="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
-amount=0.2  # 20,000,000 satoshis in BTC
-change_address=$(bitcoin-cli -rpcwallet=btrustwallet getnewaddress)
+receipient=2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP 
+transaction="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
 
-# Raw transaction to extract UTXOs from
-raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+txn_id=$(bitcoin-cli -regtest decoderawtransaction $transaction | jq -r '.txid' )
 
-# Decode transaction to get UTXO details
-decoded=$(bitcoin-cli decoderawtransaction $raw_tx)
-txid=$(echo $decoded | jq -r '.txid')
-vout=0  # Using the first output as our UTXO
-amount_utxo=$(echo $decoded | jq -r '.vout[0].value')
+# echo "getting the the vout 1..... with txn_id $txn_id"
 
-# Create raw transaction
-raw_tx=$(bitcoin-cli createrawtransaction "[{\"txid\":\"$txid\",\"vout\":$vout}]" "{\"$recipient\":$amount,\"$change_address\":$(echo "$amount_utxo - $amount - 0.00001" | bc)}")
+utxo_vout_1=$(bitcoin-cli -regtest decoderawtransaction $transaction | jq -r '.vout | .[0] | .n' )
 
-# Sign the transaction
-signed_tx=$(bitcoin-cli -rpcwallet=btrustwallet signrawtransactionwithwallet $raw_tx | jq -r '.hex')
+utxo_vout__1_value=$(bitcoin-cli -regtest decoderawtransaction $transaction | jq -r '.vout | .[0] | .value' )
 
-echo "Partially Signed Transaction: $signed_tx"
+# echo "getting the the vout 1..... with $utxo_vout_1 and value: $utxo_vout__1_value"
+
+utxo_vout_2=$(bitcoin-cli -regtest decoderawtransaction $transaction | jq -r '.vout | .[1] | .n' )
+
+utxo_vout_2_value=$(bitcoin-cli -regtest decoderawtransaction $transaction | jq -r '.vout | .[1] | .value' )
+
+# echo "getting the the vout 2..... with $utxo_vout_2 and value: $utxo_vout_2_value"
+
+bitcoin-cli -regtest -named createpsbt inputs='''[ { "txid": "'$txn_id'", "vout": '$utxo_vout_1' }, { "txid": "'$txn_id'", "vout": '$utxo_vout_2' } ]''' outputs='''{ "'$receipient'": 0.20000000 }'''
