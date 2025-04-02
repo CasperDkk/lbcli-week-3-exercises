0b34dbc169b0f8ab343c619d6522dcff16b846b9
CasperDkk <daniel.kamau8@gmail.com>
2025-04-02 16:16:01 +0300

    04.sh updated

--- submission/04.sh
+++ submission/04.sh
@@ -4,11 +4,10 @@
 # 035e11024a7fa1e0ca537e0e76b15c1631486f6611c7d8187aea8f6c1ae5d30617
 # 03ce11c7b563c3d7fae916351f234eac5089ad2e42fa9e29a94ee944e00c880c66
 
-pubkeys=(
-    "024bc198a88b69316952414af34105613c3617561a433a229100a0966cd631dd3d"
-    "035e11024a7fa1e0ca537e0e76b15c1631486f6611c7d8187aea8f6c1ae5d30617"
-    "03ce11c7b563c3d7fae916351f234eac5089ad2e42fa9e29a94ee944e00c880c66"
-)
 
-multisig=$(bitcoin-cli createmultisig 2 "$(printf '%s\n' "${pubkeys[@]}" | jq -R . | jq -s .)")
-echo "Multisig Address: $(echo $multisig | jq -r '.address')"
+pubkey1=024bc198a88b69316952414af34105613c3617561a433a229100a0966cd631dd3d
+pubkey2=035e11024a7fa1e0ca537e0e76b15c1631486f6611c7d8187aea8f6c1ae5d30617
+pubkey3=03ce11c7b563c3d7fae916351f234eac5089ad2e42fa9e29a94ee944e00c880c66
+
+address=$(bitcoin-cli -regtest -named createmultisig nrequired=2 keys='''["'$pubkey1'", "'$pubkey2'", "'$pubkey3'"]''' | jq '.address')
+echo ${address//\"/}
